26c112ad5e120887efa4dbd64d8afd5d9aea0321
M_Nayak <M_Nayak@dellteam.com>
2021-10-27 22:11:56 +0530

    Added Common file in "master"

---
+++ ConsoleApp1/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Main Branch Fix in master
+    class Common
+    {
+    }
+}
